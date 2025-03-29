c9c925f3293e2ad01f693f391b82599e5fdbf82c
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-03-29 19:45:44 +0200

    finished quiz-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import qBank from "./Components/QuestionBank";
+import Question from "./Components/Question";
+import Score from "./Components/Score";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Template</h1>
-      <p>Started template for React-project</p>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionBank: qBank,
+      currentQuestion: 0,
+      selectedOption: "",
+      score: 0,
+      quizEnd: false
+    }
+  }
+
+  handleOptionChange = (e) => {
+    this.setState({ selectedOption: e.target.value });
+  }
+
+  // function handleOptionChange(e) {
+
+  // }
+
+  // const handleOptionChange = function (r) {}
+
+  // (function () {})();
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.checkAnswer();
+    this.handleNextQuestion();
+  }
+
+  checkAnswer = () => {
+    const { questionBank, currentQuestion, selectedOption, score } = this.state;
+    if (selectedOption === questionBank[currentQuestion].answer) {
+      this.setState((prevState) => ({ score: prevState.score + 1 }));
+    }
+  };
+
+  handleNextQuestion = () => {
+    const { questionBank, currentQuestion } = this.state;
+    if (currentQuestion + 1 < questionBank.length) {
+      this.setState((prevState) => ({
+        currentQuestion: prevState.currentQuestion + 1,
+        selectedOption: ""
+      }));
+    } else {
+      this.setState({
+        quizEnd: true
+      })
+    }
+  };
+
+  render() {
+    const { questionBank, currentQuestion, selectedOption, score, quizEnd } = this.state;
+
+    return (
+      <div className="App">
+        <h1>QUIZ APP</h1>
+        {/* умова ? true : false ------- тернарний оператор*/}
+        {!quizEnd ? (
+          <Question
+            question={questionBank[currentQuestion]}
+            selectedOption={selectedOption}
+            onOptionChange={this.handleOptionChange}
+            onSubmit={this.handleFormSubmit}
+          />
+        ) : (
+          <Score
+            score={score}
+            onNextQuestion={this.handleNextQuestion}
+            className="score"
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
